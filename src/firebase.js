b1886d8a096de7573e60e8e69ce836c181110552
tweetyjxr-dotcom <tweetyjxr@gmail.com>
2026-08-29 21:23:03 +0300

    Create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBWl0ifSNFUGc6gHZb_6_61CEfpXwKkFLU",
+  authDomain: "midoyol.firebaseapp.com",
+  projectId: "midoyol",
+  storageBucket: "midoyol.firebasestorage.app",
+  messagingSenderId: "591170675503",
+  appId: "1:591170675503:web:738bb01a5fd9cb5a7837da",
+  measurementId: "G-3BZGY5Z1R9"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
